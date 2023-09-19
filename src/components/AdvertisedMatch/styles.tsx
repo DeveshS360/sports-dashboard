933833d42bc573dca5a398c147c82054bd1aa26c
devesh sharma <devesh.sharma@navi.com>
2023-09-19 18:28:32 +0530

    advertisment section UI

---
+++ src/components/AdvertisedMatch/styles.tsx
@@ -0,0 +1,66 @@
+import { styled } from 'styled-components'
+
+export const StyledAdvertisedMatchContainer = styled.div`
+  background: var(--purple);
+  padding: 40px;
+  position: relative;
+
+  * {
+    color: var(--text-color-primary);
+  }
+
+  min-height: 300px;
+  border-radius: var(--card-border-radius);
+`
+
+export const StyledAdvertisedMatchTitle = styled.div`
+  font-size: 20px;
+  margin: 10px 0 20px 0;
+  font-weight: bold;
+`
+
+export const StyledAdvertisedMatchDetail = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0 8px;
+  margin-bottom: 15px;
+
+  span {
+    font-size: 14px;
+    font-weight: lighter;
+  }
+`
+
+export const StyledAdvertisedMatchGetTicket = styled.button`
+  background: #7a4988;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 20px;
+  border: none;
+  outline: none;
+  border-radius: 10px;
+  gap: 0 8px;
+  margin-top: 30px;
+`
+
+export const StyledGetTicketArrow = styled.span`
+  text-align: center;
+  background: #9867c5;
+  border-radius: 50%;
+  flex-shrink: 0;
+  padding: 5px;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const StyledAdervtisedMatchVersus = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 40%;
+  transform: translate(-30%, 0);
+`
--- src/components/MainInfo/index.tsx
+++ src/components/MainInfo/index.tsx
@@ -5,6 +5,8 @@ import {
   StyledMainInfoGreeting,
 } from './styles'
 import { FACT_OF_THE_DAY } from 'src/constants/data/fact'
+import { AdvertisedMatch } from '../AdvertisedMatch'
+import versusUrl from 'assets/images/versus.png'
 
 export const MainInfo = () => {
   const { lastName } = USER_INFO
@@ -13,6 +15,13 @@ export const MainInfo = () => {
     <StyledMainContainer>
       <StyledMainInfoGreeting>Welcome {`${lastName}`}!</StyledMainInfoGreeting>
       <StyledMainInfoFact>{FACT_OF_THE_DAY}</StyledMainInfoFact>
+      <AdvertisedMatch
+        team1="Manchester City"
+        team2="Arsenal"
+        date="10 January, 2022"
+        venue="Old Trafford Stadium"
+        versusImgUrl={versusUrl}
+      />
     </StyledMainContainer>
   )
 }
